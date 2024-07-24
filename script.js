d1bde329de3699c653074144eead96f645a73b84
Akash Rathore <105361213+AkashRathore03@users.noreply.github.com>
2024-07-24 21:46:50 +0530

    smooth scrool

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+var navMenu = document.querySelectorAll('.nav-menu a')
+var interval;
+
+for (var i = 0; i < navMenu.length; i++) {
+   navMenu[i].addEventListener('click', function (event) {
+      event.preventDefault();
+      var targetSectionID = this.textContent.trim().toLowerCase();
+      var targetSection = document.getElementById(targetSectionID)
+      interval = setInterval(function(){
+         smoothScroll(targetSection)
+      }, 20);
+   
+   
+   });
+}
+
